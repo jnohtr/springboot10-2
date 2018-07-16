37b7cf1f0bd0703c724e9c64d64491e156685b47
b <jnohtr@gmail.com>
2018-07-16 16:09:20 -0400

    furst commit

---
+++ src/main/java/com/example/demo/SpringBoot10demoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot10demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot10demoApplication.class, args);
+	}
+}
